3a29745dca5e4373c65aee80c5c1ecee68ddd67f
Adeleye_Adeyemi <josekadel355@gmail.com>
2026-08-06 13:29:17 +0100

    Update main.tf

--- TerraformP/main.tf
+++ TerraformP/main.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "kadel" {
 resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.kadel.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
 
@@ -23,7 +23,7 @@ resource "aws_subnet" "sub1" {
 resource "aws_subnet" "sub2" {
   vpc_id                  = aws_vpc.kadel.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
 }
