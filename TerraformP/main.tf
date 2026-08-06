f40f2b2b3ccf221ca408aeb4504550389b617dda
Adeleye_Adeyemi <josekadel355@gmail.com>
2026-08-06 14:36:05 +0100

    Update main.tf

--- TerraformP/main.tf
+++ TerraformP/main.tf
@@ -96,7 +96,7 @@ resource "aws_s3_bucket_public_access_block" "joyadeleye" {
 }
 
 resource "aws_instance" "Arike1" {
-  ami                    = "ami-0734cbe7f841a2e9b"
+  ami                    = "ami-0bdc7d025135d7b49"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.secure.id]
   subnet_id              = aws_subnet.sub1.id
@@ -106,7 +106,7 @@ resource "aws_instance" "Arike1" {
 
 
 resource "aws_instance" "Arike2" {
-  ami                    = "ami-0734cbe7f841a2e9b"
+  ami                    = "ami-0bdc7d025135d7b49"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.secure.id]
   subnet_id              = aws_subnet.sub2.id
